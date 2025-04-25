1d3e11259daf08a5e57a6ed3a9a620f03bebc1fd
rakshith2000 <gowdarakshith407@gmail.com>
2025-04-25 23:23:03 +0530

    Qualification Percent Addition

--- IPL/scraper.py
+++ IPL/scraper.py
@@ -22,6 +22,8 @@ def update_points_table():
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--remote-debugging-port=9222")
+    options.add_argument("--single-process")  # Reduces memory usage
 
     driver = None
     try:
