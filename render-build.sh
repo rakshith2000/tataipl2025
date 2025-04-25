bda12b7008f9bf2c1eaacbbd3ef892abf8a13424
rakshith2000 <gowdarakshith407@gmail.com>
2025-04-25 08:16:29 +0530

    Modified gitignore and added render-build.sh

---
+++ render-build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# render-build.sh
+
+# Update package list and install required dependencies
+apt-get update
+apt-get install -y wget gnupg2
+
+# Install Chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
+apt-get update
+apt-get install -y google-chrome-stable
+
+# Verify installation
+google-chrome --version
